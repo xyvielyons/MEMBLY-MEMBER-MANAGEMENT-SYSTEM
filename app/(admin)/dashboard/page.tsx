c2345aa4ca444014b1d54cccdaaee81a38f60bba
xyvie lyons <xyvielyons@gmail.com>
2024-12-12 07:14:30 +0300

    Number of registered Members in organization chart

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
-const page = () => {
+import MemberCountChart from '@/components/cards/MemberCountChart'
+import prisma from '@/lib/prisma'
+const page = async() => {
+  const members = await prisma.user.findMany()
   return (
-    <div>page</div>
+    <div>
+      <div className="px-[16px] pt-[16px]">
+        <MemberCountChart members={members}></MemberCountChart>
+      </div>
+    </div>
   )
 }
 
